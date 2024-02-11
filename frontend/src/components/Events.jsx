eeb0682b04eecd6a180b71fa39a2962243f1b95a
MSP20086 <mihirpandit04@gmail.com>
2024-02-11 12:16:08 +0530

    added some

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -4,29 +4,30 @@ import { Button, Spinner } from 'flowbite-react';
 import axios from 'axios';
 import '../styles/colorgradient.css';
 import srcimg from '../images/event.jpg';
+import './News.css'
 
 
 export default function Events({ locationName, index }) {
-  const [news, setNews] = useState([]);
+  // const [news, setNews] = useState([]);
   const [events, setEvents] = useState([]);
   const City = locationName;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const newsResponse = await axios.get(`https://newsapi.org/v2/everything?q=${City}&apiKey=be08998cf6b34115895517e0fbde1df9`);
-        const limitedNews = newsResponse.data.articles.slice(0, 15);
-        setNews(limitedNews);
+        // const newsResponse = await axios.get(`https://newsapi.org/v2/everything?q=${City}&apiKey=be08998cf6b34115895517e0fbde1df9`);
+        // const limitedNews = newsResponse.data.articles.slice(0, 15);
+        // setNews(limitedNews);
 
         const eventsResponse = await axios.request({
           method: 'GET',
           url: 'https://real-time-events-search.p.rapidapi.com/search-events',
           params: {
-            query: `Concerts in ${City}`,
+            query: `${City}`,
             start: '0'
           },
           headers: {
-            'X-RapidAPI-Key': 'fdebd55b92msh21517247185c7e1p13b3cejsn928ff4381e55',
+            'X-RapidAPI-Key': 'fdf27181b2msh518ba6f8db6838dp14cd4djsnbef46d023756',
             'X-RapidAPI-Host': 'real-time-events-search.p.rapidapi.com'
           }
         });
@@ -54,7 +55,7 @@ export default function Events({ locationName, index }) {
     <div>
       <div className='w-3/4 m-auto mb-20'>
         <strong className='mx-auto justify-center items-center text-5xl text-indigo-700'>Events</strong>
-        <div className='mt-10 events-carousel'>
+        <div className='mt-10 events-carousel h-full'>
           {events.length === 0 ? (
             <div className='flex items-center justify-center h-full'>
               <Spinner aria-label="Default status example" className='m-auto flex items-center justify-center' size='xl' color='purple' />
@@ -62,18 +63,20 @@ export default function Events({ locationName, index }) {
           ) : (
             events.map((event, index) => (
               <animated.div key={index} style={props} className='bg-white border shadow text-black rounded-xl event-card h-full grow'>
-                <div className='rounded-t-xl flex justify-center items-center h-full grow'>
+                <div className='rounded-t-xl flex justify-center items-center'>
                   {event.thumbnail ? (
-                    <img src={event.thumbnail} alt='' className='h-auto w-full rounded-xl' />
+                    <img src={event.thumbnail} alt='' className='h-[300px] w-full rounded-xl' />
                   ) : (
                     <img src={srcimg} alt='' className='h-auto w-full rounded-xl' />
                   )}
                 </div>
-                <div className='flex flex-col justify-center items-center gap-4 p-4 h-full grow'>
+                <div className='flex flex-col justify-center items-center gap-4 p-4 h-[300px] grow'>
                   <h3 className='text-xl font-semibold'>{event.name}</h3>
                   <p>{event.description ? event.description.slice(0, 100) : null}...</p>
-                  <p>Start Time: {event.start_time ? event.start_time.slice(0, 10) : null}</p>
-                  <p>End Time: {event.end_time ? event.end_time.slice(0, 10) : null}</p>
+                  <div>
+                    <p>Start Time: {event.start_time ? event.start_time.slice(0, 10) : null}</p>
+                    <p>End Time: {event.end_time ? event.end_time.slice(0, 10) : null}</p>
+                  </div>
                 </div>
               </animated.div>
             )))
@@ -81,7 +84,7 @@ export default function Events({ locationName, index }) {
         </div>
       </div>
 
-      <div className='w-3/4 m-auto mb-20'>
+      {/* <div className='w-3/4 m-auto mb-20'>
         <strong className='mx-auto justify-center items-center text-5xl text-indigo-700'>News</strong>
         <div className='mt-10 news-carousel'>
           {news.length !== 0 ? (news.map((article, index) => (
@@ -115,7 +118,7 @@ export default function Events({ locationName, index }) {
             </div>
           )}
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
